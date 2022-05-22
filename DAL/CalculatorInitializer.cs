40f278c1049a85193cf9fcc59b9c8a278eb1ced5
TomaszMyszak <t.myszak@hotmail.com>
2022-05-22 19:17:48 +0200

    try to Add DataBasse to SQLSERVER

---
+++ DAL/CalculatorInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ProductionManagment.Models;
+
+namespace ProductionManagment.DAL
+{
+    public class CalculatorInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<CalculatorContext>
+    {
+        protected override void Seed(CalculatorContext context)
+            var citys = new List<City>
+            {
+                new citys{Id="1", Name="Tarnowskie Góry", TransportCost="100", CostOfWorkingHour ="120"},
+                new citys{Id="2", Name="Katowice", TransportCost="110", CostOfWorkingHour ="140"},
+                new citys{Id="3", Name="Glwice", TransportCost="150", CostOfWorkingHour ="120"},
+                new citys{Id="4", Name="Warszawa", TransportCost="200", CostOfWorkingHour ="180"},
+                new citys{Id="5", Name="Poznań", TransportCost="250", CostOfWorkingHour ="100"},
+                new citys{Id="6", Name="Szczeciń", TransportCost="300", CostOfWorkingHour ="250"},
+                new citys{Id="7", Name="Lublin", TransportCost="280", CostOfWorkingHour ="80"},
+            };
+
+            citys.ForEach(s => context.City.Add(s));
+            context.SaveChanges();
+
+            var Modules = new List<Module>
+            {
+                new Modules{ Id = " 1"}
+            }
+    }
+}
--- Models/CalculatorContext.cs
+++ Models/CalculatorContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,16 @@ namespace ProductionManagment.Models
 {
     public class CalculatorContext : DbContext
     {
-       
-        public CalculatorContext(String connectionString)
+        public CalculatorContext()
             : base("name=ConnectionString") { }
 
         public DbSet<SearchHistory> SearchHistory { get; set; }
         public DbSet<Module> Module { get; set; }
         public DbSet<City> City { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
