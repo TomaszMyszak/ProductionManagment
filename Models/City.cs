d2d495c318b0a72a84523aecea0c53465fa7164f
TomaszMyszak <t.myszak@hotmail.com>
2022-05-22 13:22:10 +0200

    enter the connectionString

---
+++ Models/City.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProductionManagment.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double TransportCost { get; set; }
+
+        public double CostOfWorkingHour { get; set; }
+
+        public virtual SearchHistory SearchHistory { get; set; }
+    }
+    
+}
